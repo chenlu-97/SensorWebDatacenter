4c12e9c2309521a401cb383f1468737148f4b819
chenlu-97 <1152214157@qq.com>
2021-09-07 16:57:48 +0800

    提交代码

--- datacenter-online-service/src/main/java/com/sensorweb/datacenteronlineservice/feign/ObsFeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.sensorweb.datacenteronlineservice.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "sos-obs-service")
-public interface ObsFeignClient {
-
-}
